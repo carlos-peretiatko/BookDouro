3990f9dabd16b06a328c7796bc1d886ff3221f67
Carlos <carloseduardoperetiatko07@gmail.com>
2025-04-02 17:06:04 -0300

    up!! 02/04

---
+++ BookDouro/FrontEnd/src/pages/profile.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const eyeIcons = document.querySelectorAll('.icon-eye');
+
+    eyeIcons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            alert('Esta funcionalidade será implementada futuramente.');
+        });
+    });
+});
